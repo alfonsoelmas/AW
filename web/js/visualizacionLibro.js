8d74f4411a83517b9217520cfd88cfbfa58bfe05
masju96 <jumas@ucm.es>
2017-04-12 11:56:16 +0200

    Añadido visualizacionLibro, necesita arreglar el footer

---
+++ web/js/visualizacionLibro.js
@@ -0,0 +1,7 @@
+document.getElementById("logInButton").onclick = function(){
+  location.href='login.html';
+};
+
+document.getElementById("registerButton").onclick = function(){
+  location.href="registro.html";
+};
