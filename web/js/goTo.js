a1d1d0ecdc532b613e4a8ab650d12494586fe161
eliiags <eaguero@ucm.es>
2017-04-24 13:15:40 +0200

    acbado, falta depurar

--- web/js/goTo.js
+++ web/js/goTo.js
@@ -1,5 +1,5 @@
 
-/*document.getElementById("logInButton").onclick = function(){
+document.getElementById("logInButton").onclick = function(){
   location.href='login.html';
 };
 
@@ -10,18 +10,3 @@ document.getElementById("registerButton").onclick = function(){
 document.getElementById("leer").onclick = function(){
   location.href="visualizacionContenidoLibro.html";
 };
-*/
-
-document.getElementById("blabla").onclick = function(){
-  location.href='login.html';
-};
-
-/*
-document.getElementsByClassName("vista_libro").onclick = function(){
-  location.href="edicion.html";
-};
-
-document.getElementsByClassName("vista_usuario").onclick = function(){
-  location.href="../vistaUsuario.html";
-};
-*/
