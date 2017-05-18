36b38b86f16f5494357f59f5dbb81c3869e8163a
gjimcor <gjimcor@hotmail.com>
2017-05-18 12:36:37 +0200

    Update genera_pie.php

--- web/php/funciones/genera_pie.php
+++ web/php/funciones/genera_pie.php
@@ -1,4 +1,8 @@
 <?php
+// Genera el pie de página de la web.
+
+if(!isset($_SESSION))
+	session_start();
 
 echo "<footer class='container-fluid text-center'>";
 	echo "<p><a id='color-defecto' href='aboutUs.html'>Paper Dreams 2017 - Un proyecto realizado por el grupo Bi-Inestables</a></p>";
