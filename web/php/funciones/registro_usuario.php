17ec5ef15c976a36f3374e754a89bbec6adcb841
gjimcor <gjimcor@hotmail.com>
2017-05-22 12:38:14 +0200

    Update registro_usuario.php
    Casi terminado. Falta poner los errores directamente en la página de registro.php

--- web/php/funciones/registro_usuario.php
+++ web/php/funciones/registro_usuario.php
@@ -9,7 +9,7 @@
 		<link rel="stylesheet" href="../../css/login.css">
 	</head>
 	<body>
-		<?php include("genera_cabecera.php"); ?>
+		<?php require_once("genera_cabecera.php"); ?>
 		
 		<div class="container-fluid text-center">    
 			<div class="row content">
@@ -22,14 +22,6 @@
 						<div class="panel-body">
 							<div class="col-sm-6 text-center content">
 								<?php registra_usuario(); ?>
-							</div>
-							<div class="col-sm-6 text-center content">
-								<div id="inicio_sesion">
-									<span>O si ya tienes una cuenta: </span>
-									<div class="row margin-normal">   
-										<button class="btn btn-default dropdown-toggle engordar redondear" type="button" id="logInButton">Iniciar sesión</button>
-									</div>
-								</div>
 							</div>
 						</div>
 					</div>
@@ -48,59 +40,55 @@
 <?php
 	// Registro de un usuario en la BD.
 	function registra_usuario() {
-		session_start();
-		include('../config/connection.php');
-		include('../config/filtrado.php');
+		require_once('consulta.php');
+		require_once('filtrado_entrada.php');
 
 		// COMPROBACIONES DE SEGURIDAD
+		array_walk($_REQUEST, 'limpiarCadena');	
+
 		$nombre = htmlspecialchars(trim(strip_tags($_REQUEST["name"])));
-		filtrar($nombre);
 		$usuario = htmlspecialchars(trim(strip_tags($_REQUEST["user"])));
-		filtrar($usuario);
 		$edad = htmlspecialchars(trim(strip_tags($_REQUEST["age"])));
-		filtrar($edad);
 		$email = htmlspecialchars(trim(strip_tags($_REQUEST["email"])));
-		filtrar($email);
 		$password = htmlspecialchars(trim(strip_tags($_REQUEST["password"])));
-		filtrar($password);
 		$confirm_password = htmlspecialchars(trim(strip_tags($_REQUEST["confirm_password"])));
-		filtrar($confirm_password);
-		$mysqli = conectar();
-	
+		
+		$password_hased = password_hash($password, PASSWORD_DEFAULT);
+
 		if(empty($nombre) || empty($usuario) || empty($edad) || empty($email) || empty($password) || empty($confirm_password) ||
 	   	   !preg_match('/^[^@\s]+@([a-z0-9]+\.)+[a-z]{2,}$/i', $email) ||
 	   	   !is_numeric($edad) ||
 	   	   strlen($password) < 8 ||
 	   	   $password != $confirm_password) {
 			echo "<p>Se ha producido un error al enviar los datos del formulario.</p>";
-			echo "<button class='btn btn-default dropdown-toggle engordar redondear' type='button' id='registerButton'>Volver al formulario de registro</button>";
+			echo "<a href='../../registro.php'><button class='btn btn-default dropdown-toggle engordar redondear' type='button' id='button'>Volver al formulario de registro</button></a>";
 		}
 		else {
 			// Comprobar si el usuario ya está registrado en la BD.
 			$query = "SELECT * FROM usuarios WHERE usuario LIKE '$usuario'";
-			$resultado = realiza_consulta($mysqli, $query);
+			$resultado = consulta($query);
 
 			if($resultado->num_rows != 0) {
 				echo "<p>El nombre de usuario ya existe. Prueba con otro.</p>";
-				echo "<button class='btn btn-default dropdown-toggle engordar redondear' type='button' id='registerButton'>Volver al formulario de registro</button>";
+				echo "<a href='../../registro.php'><button class='btn btn-default dropdown-toggle engordar redondear' type='button' id='button'>Volver al formulario de registro</button></a>";
 			}
 			else {
 				// Comprobar si el email ya está registrado en la BD.
 				$query = "SELECT * FROM usuarios WHERE email LIKE '$email'";
-				$resultado = realiza_consulta($mysqli, $query);
+				$resultado = consulta($query);
 				if($resultado->num_rows != 0) {
 					echo "<p>El email introducido ya está registrado. Prueba con otro.</p>";
-					echo "<button class='btn btn-default dropdown-toggle engordar redondear' type='button' id='registerButton'>Volver al formulario de registro</button>";
+					echo "<a href='../../registro.php'><button class='btn btn-default dropdown-toggle engordar redondear' type='button' id='button'>Volver al formulario de registro</button></a>";
 				}
 				else {
 					// Registrar el nuevo usuario en la BD.
-					$query = "INSERT INTO usuarios (usuario, nombre, email, pass, edad) VALUES ('$usuario', '$nombre', '$email', '$password', '$edad')";
-					realiza_consulta($mysqli, $query);
-					echo "<p>¡Te has registrado correctamente!";
+					$query = "INSERT INTO usuarios (usuario, nombre, email, pass, edad) VALUES ('$usuario', '$nombre', '$email', '$password_hased', '$edad')";
+					consulta($query);
+					session_start();
+					$_SESSION['name'] = $nombre;
+					header('Location: ../../index.php');
 				}
 			}
 		}
-
-		cerrar_conexion($mysqli);
 	}
 ?>
