4fcd107c88776c5b15b2eb9a7c4137419fefaf72
Experimentando y aprendiendo <alfonso_soria_m@hotmail.com>
2017-05-22 09:58:43 +0200

    poco +
    hoy lunes acabo categorias y resultados de busqueda

--- web/categorias.php
+++ web/categorias.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html>
 <html lang="es">
+<?php  sesion_start(); ?>
+
 <head>
   <title>Categorías - Paper Dreams</title>
   <meta charset="utf-8">
@@ -9,59 +11,48 @@
 </head>
 
 <body>
-	<div class="jumbotron" id="banner">
-		<div class="text-left">
-			<div class="col-sm-2">
-				<img id="logo" alt="" src="img/logo1.png" class="img-responsive">
-			</div>
-			<div class="col-sm-10">
-				<p class="h1">Paper Dreams</p>      
-				<p>De tus sueños al papel</p>
-			</div>
-			</div>
-				<div id="breadcum-div">
-				<ol class="breadcrumb" id="breadcum">
-					<li><a href="index.html">Inicio</a></li>
-					<li class="active">Obras</li>
-				</ol>
-		</div>
-	</div>
-
-	<nav class="navbar navbar-inverse">
-		<div class="container-fluid">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>                        
-				</button>
-				<a class="navbar-brand" href="index.html">Inicio</a>
-			</div>
-			<div class="collapse navbar-collapse" id="myNavbar">
-				<ul class="nav navbar-nav">
-					<li><a class="actual" href="#">Categorías</a></li>
-					<li><a href="contacto.html">Contacto</a></li>
-					<li><a href="aboutUs.html">About Us</a></li>
-				</ul>
-				<div class="nav navbar-nav navbar-right">
-					<form class="navbar-form navbar-left input-group-btn" role="search" action="result-busqueda.html">
-						<div class="form-group">
-							<input type="text" class="form-control" placeholder="Search">
-						</div>
-						<button id="buscador" type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span>Buscar</button>
-					</form>
-				</div>
-			</div>
-		</div>
-	</nav>
+	<?php
+		$pagina_actual="categorias";
+		include("php/funciones/genera_cabecera.php");
+		include("php/config/connection.php");
+	?>
+	
 
 	<div class="container-fluid text-center">    
 		<div class="row content">
 			<div class="col-sm-10 text-left"> 
 				<div class="panel panel-default">
+
+					<?php 
+						//Lanzamos una query para obtener las categorias
+
+						$conn = conectar();
+						//Necesito obtener las categorias existentes (sin repeticion).
+						$query = "SELECT DISTINCT categoria FROM libros";
+
+
+						$consulta 	= 	realiza_consulta($conn, $query);
+						$filas 		= 	mysqli_num_rows($consulta);
+
+						//Hacemos un for del numero de categorias
+						for($i = 0; $i<$filas; $i++){
+
+
+							//TODO
+							//Pintamos titulo
+							//Lanzamos query de esa categoria ordenados por votos
+							//Las pintamos y hacemos el scroll de alguna forma TODO
+
+
+						}
+
+					?>
+
 					<div class="panel-heading">
 						<p class="panel-title h3">Terror</p>
 					</div>
+
+
 					<div class="panel-body">
 						<div class="row">
 							<div class="col-sm-6 col-md-3">
@@ -115,6 +106,8 @@
 						</div>
 					</div>  
 				</div>
+
+
 				<div class="panel panel-default">
 					<div class="panel-heading">
 						<p class="panel-title h3">Romance </p>
@@ -298,10 +291,7 @@
 		</div>
 	</div>
 
-
-	<footer class="container-fluid text-center">
-		<p><a id="color-defecto" href="aboutUs.html">Paper Dreams 2017 - Un proyecto realizado por el grupo Bi-Inestables</a></p>
-	</footer>
+	<?php include("php/funciones/genera_pie.php"); ?>
 
 	<!--Scripts-->
 	<script type="text/javascript" src="js/goTo.js"></script>
