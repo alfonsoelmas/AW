192d3c3d4ce929112cce97510947f15230e70eea
gjimcor <gjimcor@hotmail.com>
2017-05-23 01:01:26 +0200

    Update miPerfil.php
    Cabecera, footer y panel derecho dinámicos en miPerfil.php

--- web/miPerfil.php
+++ web/miPerfil.php
@@ -9,52 +9,10 @@
 </head>
 
 <body>
-
-    <div class="jumbotron" id="banner">
-		<div class="text-left">
-			<div class="col-sm-2">
-				<img id="logo" alt="logo" class="img-responsive"   src="img/logo1.png">
-			</div>
-			<div class="col-sm-10">
-			<p class="h1">Paper Dreams</p>      
-				<p>De tus sueños al papel</p>
-			</div>
-		</div>
-		<div id="breadcum-div">
-			<ol class="breadcrumb" id="breadcum" >
-				<li><a href="index.html">Inicio</a></li>
-				<li class="active">Mi Perfil</li>
-			</ol>
-		</div>
-	</div>
-
-	<nav class="navbar navbar-inverse">
-		<div class="container-fluid">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>                        
-				</button>
-				<a class="navbar-brand" href="index.html">Inicio</a>
-			</div>
-			<div class="collapse navbar-collapse" id="myNavbar">
-				<ul class="nav navbar-nav">
-					<li><a href="categorias.html">Categorías</a></li>
-					<li><a href="contacto.html">Contacto</a></li>
-					<li><a href="aboutUs.html">About Us</a></li>
-				</ul>
-				<div class="nav navbar-nav navbar-right">
-					<form class="navbar-form navbar-left input-group-btn" role="search" action="result-busqueda.html">
-						<div class="form-group">
-							<input type="text" class="form-control" placeholder="Search">
-						</div>
-						<button id="buscador" type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span>Buscar</button>
-					</form>
-				</div>
-			</div>
-		</div>
-	</nav>
+	<?php
+		$pagina_actual="Mi perfil";
+		include("php/funciones/genera_cabecera.php");
+	?>
 
     <!--Perfil-->
     <div class="container-fluid text-center">    
@@ -80,21 +38,14 @@
                     </div>
                 </div>
             </div>
-            <div class="col-sm-2 sidenav">
-              <div class="text-center">
-                  <div class="dropdown text-left">
-                      <p><a href="misObras.html"><span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span> Mis obras</a></p>
-                      <p><a href="misSeguidores.html"><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> Mis seguidores</a></p>
-                      <p><a href="siguiendo.html"><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> Siguiendo</a></p>
-                  </div>
-              </div>
-            </div>
+            <?php
+				$pagina_actual="Mi perfil";
+				include("php/funciones/genera_bloque_derecha.php");
+			?>
         </div>
     </div>
 
-    <footer class="container-fluid text-center">
-		<p><a id="color-defecto" href="aboutUs.html">Paper Dreams 2017 - Un proyecto realizado por el grupo Bi-Inestables</a></p>
-    </footer>
+    <?php include("php/funciones/genera_pie.php"); ?>
 	
 	<!--Script-->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
