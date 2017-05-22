17d6d3d04b32ae64b5ef68c03ab9cf3da58b3996
Experimentando y aprendiendo <alfonso_soria_m@hotmail.com>
2017-05-22 23:35:16 +0200

    cambios
    falta apañar cosas

--- web/categorias.php
+++ web/categorias.php
@@ -8,12 +8,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <link rel="stylesheet" href="css/estilo2.css">
+
 </head>
 
 <body>
 	<?php
 		$pagina_actual="categorias";
-		include("php/funciones/genera_cabecera.php");
+		<?php require_once('php/funciones/genera_cabecera.php'); genera_cabecera('$pagina_actual');?>
 		include("php/config/connection.php");
 	?>
 	
@@ -33,256 +34,68 @@
 
 						$consulta 	= 	realiza_consulta($conn, $query);
 						$filas 		= 	$consulta->num_rows;
-						$datosConsulta = $consulta->fetch_array(MYSQL_NUM);
+						$datosConsulta = $consulta->fetch_assoc();
 
 						//Hacemos un for del numero de categorias
 						for($i = 0; $i<$filas; $i++){
 
 							//Pintamos titulo
-						?>
+							?>
 							<div class="panel-heading">
-								<p class="panel-title h3"><?php echo $datosConsulta[i]; ?></p>
-							</div>
+								<?php 	echo '<a href="result-busqueda.php?categoria='.$datosConsulta["categoria"].'">'?>
+									<p class="panel-title h3"><?php echo $datosConsulta["categoria"]; ?></p> 
+								</a>
+								<span class="badge"><?php echo $filas?></span></p>
+
+							</div>					
+							<div class="panel-body">
+								<div class="row">
 						<?php 
 							//Lanzamos query de esa categoria ordenados por votos
-							$query = "SELECT titulo, portada FROM libros WHERE categoria=$jugos[0]".PHP_EOL;
+							$query = "SELECT id_libro, titulo, portada FROM libros WHERE categoria=$jugos[0]".PHP_EOL;
+
+
+							$consulta 	= 	realiza_consulta($conn, $query);
 							$nResultados = 	$consulta->num_rows;//numero de resultados
-							$datosResultados =  $consulta->fetch_array(MYSQL_NUM);
 
-							//Las pintamos y hacemos el scroll de alguna forma TODO
 
+
+							//Las pintamos pero dejamos ocultas? TODO HACERLO ASI: http://lineadecodigo.com/jquery/cargar-contenido-con-el-scroll-usando-jquery/
+							while($row = $consulta->fetch_assoc()){
+								?>
+								<div class="col-sm-6 col-md-3">
+									<div class="thumbnail efecto-redondo">
+										<?php 	echo '<a href="visualizacionLibro.php?id='.$row["id_libro"].'">'
+												echo '<img alt="" src="'.$row["portada"].'" class="img-responsive imgP">'
+												echo '<div class="caption">'
+												echo '<p>'.$row["titulo"].'</p>'
+										?>
+											</div>
+										</a>
+									</div>
+								</div>
+
+							<?php
+							}
+
+							?>
+										<div class="row">
+										<div class="col-sm-12 text-center">
+											<div class="btn-group" role="group" aria-label="...">
+												<button id=<?php echo 'bn'.$i ?> type="button" class="btn btn-default"><span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>Prev</button>
+												<button id=<?php echo 'bn'.$i ?> type="button" class="btn btn-default">Next<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span></button>
+											</div>
+										</div>
+									</div>
+								</div>  
+							</div>
+							<?php
 
 						}
 
 					?>
 
-					<div class="panel-body">
-						<div class="row">
-							<div class="col-sm-6 col-md-3">
-								<div class="thumbnail efecto-redondo">
-									<a href="">
-										<img alt="" src="img/logo2.png" class="img-responsive imgP">
-										<div class="caption">
-											<p>Title 1</p>
-										</div>
-									</a>
-								</div>
-							</div>
-							<div class="col-sm-6 col-md-3">
-								<div class="thumbnail efecto-redondo">
-									<a href="">
-										<img alt="" src="img/logo2.png" class="img-responsive imgP">
-										<div class="caption">
-											<p>Title2</p>
-										</div>
-									</a>
-								</div>
-							</div>
-							<div class="col-sm-6 col-md-3">
-								<div class="thumbnail efecto-redondo">
-									<a href="">
-										<img alt="" src="img/logo2.png" class="img-responsive imgP">
-										<div class="caption">
-											<p>Title3</p>
-										</div>
-									</a>
-								</div>
-							</div>
-							<div class="col-sm-6 col-md-3">
-								<div class="thumbnail efecto-redondo">
-									<a href="">
-										<img alt="" src="img/logo2.png" class="img-responsive imgP">
-										<div class="caption">
-											<p>Title4</p>
-										</div>
-									</a>
-								</div>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-sm-12 text-center">
-								<div class="btn-group" role="group" aria-label="...">
-									<button type="button" class="btn btn-default"><span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>Prev</button>
-									<button type="button" class="btn btn-default">Next<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span></button>
-								</div>
-							</div>
-						</div>
-					</div>  
-				</div>
-
-
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						<p class="panel-title h3">Romance </p>
-					</div>
-					<div class="panel-body">
-						<div class="row">
-							<div class="col-sm-6 col-md-3">
-								<div class="thumbnail efecto-redondo">
-									<a href="">
-										<img alt="" src="img/logo2.png" class="img-responsive imgP">
-										<div class="caption">
-											<p>Title 1</p>
-										</div>
-									</a>
-								</div>
-							</div>
-							<div class="col-sm-6 col-md-3">
-								<div class="thumbnail efecto-redondo">
-									<a href="">
-										<img alt="" src="img/logo2.png" class="img-responsive imgP">
-										<div class="caption">
-											<p>Title 2</p>
-										</div>
-									</a>
-								</div>
-							</div>
-							<div class="col-sm-6 col-md-3">
-								<div class="thumbnail efecto-redondo">
-									<a href="">
-										<img alt="" src="img/logo2.png" class="img-responsive imgP">
-										<div class="caption">
-											<p>Title 3</p>
-										</div>
-									</a>
-								</div>
-							</div>
-							<div class="col-sm-6 col-md-3">
-								<div class="thumbnail efecto-redondo">
-									<a href="">
-										<img alt="" src="img/logo2.png" class="img-responsive imgP">
-										<div class="caption">
-											<p>Title 4</p>
-										</div>
-									</a>
-								</div>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-sm-12 text-center">
-								<div class="btn-group" role="group" aria-label="...">
-									<button type="button" class="btn btn-default"><span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>Prev</button>
-									<button type="button" class="btn btn-default">Next<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span></button>
-								</div>
-							</div>
-						</div>
-					</div>  
-				</div>
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						<p class="panel-title h3">Ciencia-Ficción</p>
-					</div>
-					<div class="panel-body">
-						<div class="row">
-							<div class="col-sm-6 col-md-3">
-								<div class="thumbnail efecto-redondo">
-									<a href="">
-										<img alt="" src="img/logo2.png" class="img-responsive imgP">
-										<div class="caption">
-											<p>Title 1</p>
-										</div>
-									</a>
-								</div>
-							</div>
-							<div class="col-sm-6 col-md-3">
-								<div class="thumbnail efecto-redondo">
-									<a href="">
-										<img alt="" src="img/logo2.png" class="img-responsive imgP">
-										<div class="caption">
-											<p>Title 2</p>
-										</div>
-									</a>
-								</div>
-							</div>
-							<div class="col-sm-6 col-md-3">
-								<div class="thumbnail efecto-redondo">
-									<a href="">
-										<img alt="" src="img/logo2.png" class="img-responsive imgP">
-										<div class="caption">
-											<p>Title 3</p>
-										</div>
-									</a>
-								</div>
-							</div>
-							<div class="col-sm-6 col-md-3">
-								<div class="thumbnail efecto-redondo">
-									<a href="">
-										<img alt="" src="img/logo2.png" class="img-responsive imgP">
-										<div class="caption">
-											<p>Title 4</p>
-										</div>
-									</a>
-								</div>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-sm-12 text-center">
-								<div class="btn-group" role="group" aria-label="...">
-									<button type="button" class="btn btn-default"><span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>Prev</button>
-									<button type="button" class="btn btn-default">Next<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span></button>
-								</div>
-							</div>
-						</div>
-					</div>  
-				</div>
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						<p class="panel-title h3">Bocetos</p>
-					</div>
-					<div class="panel-body">
-						<div class="row">
-							<div class="col-sm-6 col-md-3">
-								<div class="thumbnail efecto-redondo">
-								<a href="">
-								<img alt="" src="img/logo2.png" class="img-responsive imgP">
-								<div class="caption">
-								<p>Title 1</p>
-								</div>
-								</a>
-								</div>
-							</div>
-							<div class="col-sm-6 col-md-3">
-								<div class="thumbnail efecto-redondo">
-								<a href="">
-								<img alt="" src="img/logo2.png" class="img-responsive imgP">
-								<div class="caption">
-								<p>Title2</p>
-								</div>
-								</a>
-								</div>
-							</div>
-							<div class="col-sm-6 col-md-3">
-								<div class="thumbnail efecto-redondo">
-									<a href="">
-										<img alt="" src="img/logo2.png" class="img-responsive imgP">
-										<div class="caption">
-											<p>Title3</p>
-										</div>
-									</a>
-								</div>
-							</div>
-							<div class="col-sm-6 col-md-3">
-								<div class="thumbnail efecto-redondo">
-									<a href="">
-										<img alt="" src="img/logo2.png" class="img-responsive imgP">
-										<div class="caption">
-											<p>Title4</p>
-										</div>
-									</a>
-								</div>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-sm-12 text-center">
-								<div class="btn-group" role="group" aria-label="...">
-									<button type="button" class="btn btn-default"><span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>Prev</button>
-									<button type="button" class="btn btn-default">Next<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span></button>
-								</div>
-							</div>
-						</div>
-					</div>  
-				</div>
-			</div>
+							
 			
 			<div class="col-sm-2 sidenav">
 				<div class="dropdown text-left">
@@ -300,6 +113,12 @@
 	<script type="text/javascript" src="js/goTo.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script>
+		function siguiente(){
+
+		}
+
+	</script>
 
 </body>
 </html>
