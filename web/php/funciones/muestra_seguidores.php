4b379747d089d6db0460978f19ed690095e4b5cd
gjimcor <gjimcor@hotmail.com>
2017-05-23 12:44:20 +0200

    Add files via upload

---
+++ web/php/funciones/muestra_seguidores.php
@@ -0,0 +1,48 @@
+<?php
+
+	//require_once('consulta.php');
+	//$query = // la query que sea "SELECT id_seguidor FROM seguidores WHERE id_seguido LIKE $_SESSION['name']";
+
+	echo "
+	<div class='col-sm-6 col-md-3'>
+		<div class='thumbnail efecto-redondo'>
+			<a href='vistaUsuario.php'>
+				<img src='img/logo2.png' alt='' class='imgP'>
+				<div class='caption'>
+					<p>Seguidor 1</p>
+				</div>
+			</a>
+		</div>
+	</div>
+	<div class='col-sm-6 col-md-3'>
+		<div class='thumbnail efecto-redondo'>
+			<a href='vistaUsuario.php'>
+				<img src='img/logo2.png' alt='' class='imgP'>
+				<div class='caption'>
+					<p>Seguidor 2</p>
+				</div>
+			</a>
+		</div>
+	</div>
+	<div class='col-sm-6 col-md-3'>
+		<div class='thumbnail efecto-redondo'>
+			<a href='vistaUsuario.php'>
+				<img class='imgP' alt='' src='img/logo2.png'>
+				<div class='caption'>
+					<p>Seguidor 3</p>
+				</div>
+			</a>
+		</div>
+	</div>
+	<div class='col-sm-6 col-md-3'>
+		<div class='thumbnail efecto-redondo'>
+			<a href='vistaUsuario.php'>
+				<img src='img/logo2.png' alt='' class='imgP'>
+				<div class='caption'>
+					<p>Seguidor 4</p>
+				</div>
+			</a>
+		</div>
+	</div>";
+
+?>
