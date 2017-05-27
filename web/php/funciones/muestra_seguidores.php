7ecc9957cd6077cc05e396628dcaa794430cba40
gjimcor <gjimcor@hotmail.com>
2017-05-27 03:49:04 +0200

    Update muestra_seguidores.php

--- web/php/funciones/muestra_seguidores.php
+++ web/php/funciones/muestra_seguidores.php
@@ -13,7 +13,7 @@
 		$inicio = ($pagina - 1) * $tamanio_pagina;
 	}
 
-	require_once($_SERVER['DOCUMENT ROOT']."/web/php/funciones/consulta.php");
+	require_once($_SERVER['DOCUMENT_ROOT']."/web/php/funciones/consulta.php");
 
 	// Obtener todos los identificadores de todos los usuarios a mostrar que siguen al usuario conectado.
 	$query = "SELECT id_seguidor FROM seguidores WHERE id_seguido LIKE ".$_SESSION['usuario_actual']." LIMIT ".$inicio.",".$tamanio_pagina;
