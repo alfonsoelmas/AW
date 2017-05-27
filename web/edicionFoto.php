c2e50b287a979e1404cd9f85628ccc4a555be347
eliiags <eaguero@ucm.es>
2017-05-28 00:27:32 +0200

    Update edicionFoto.php
    editado edicionFoto

--- web/edicionFoto.php
+++ web/edicionFoto.php
@@ -16,33 +16,32 @@
 	?>
 
 	<div class="container-fluid text-center">    
-		<div class="col-sm-10 text-left"> 
-			<div class="panel panel-default margen-top">
-				<div class="panel-heading">
-					<p class=" h3 panel-title">Edición de foto</p>
-				</div>
-				<div class="panel-body">
-					<div class="row">
-						<div class="col-sm-3 text-center">
-							<img class="img-edicion" alt="" src="img/portadas/portada.png">
-							<button type="button" class="btn btn-info margen-top">Editar imagen</button>
-						</div>
-						<div class="col-sm-9 text-left"> 
-							<form method="post" action="">
-								<span class="input-group-addon glyphicon glyphicon-text-size">Título</span>
-								<input type="text" class="form-control" id="titulo" name="title" placeholder="Escribe aquí el título"/>
-								<br>
-								<span class="input-group-addon glyphicon glyphicon-text-size">Categorías</span>
-								<input type="text" class="form-control" id="categoria" name="title" placeholder="Escribe aquí la categoría"/>
-							</form>
-						</div>
+		<form action="php/funciones/editar_foto.php" method="POST" enctype="multipart/form-data">
+			<div class="col-sm-10 text-left"> 
+				<div class="panel panel-default margen-top">
+					<div class="panel-heading">
+						<p class=" h3 panel-title">Edición de foto</p>
 					</div>
-				</div>  
+					<div class="panel-body">
+						<div class="row">
+							<div class="col-sm-4">
+								<input id="imagen" name="imagen" type="file" class="file">
+							</div>
+							<div class="col-sm-8 text-left"> 
+								<span class="input-group-addon glyphicon glyphicon-text-size">Título</span>
+								<input type="text" class="form-control" id="titulo" name="titulo" placeholder="Escribe aquí el título"/>
+								<br>
+								<span class="input-group-addon glyphicon glyphicon-text-size">Descripción</span>
+								<input type="text" class="form-control" id="categoria" name="desc" placeholder="Escribe aquí alguna descripción"/>
+							</div>
+						</div>
+					</div>  
+				</div>
+				<div class="col-sm-12 text-center">
+					<button type="submit" class="btn btn-warning margen-bottom">Guardar cambios realizados</button>
+				</div>
 			</div>
-			<div class="col-sm-12 text-center">
-				<button type="button" class="btn btn-warning margen-bottom">Guardar cambios realizados</button>
-			</div>
-		</div>
+		</form>
 		<?php
 			$pagina_actual="EdiciónFoto";
 			require_once($_SERVER['DOCUMENT_ROOT'] ."/web/php/funciones/genera_bloque_derecha.php");
@@ -54,10 +53,10 @@
 	?>
 
 	<!--Scripts-->
-	<script type="text/javascript" src="js/goTo.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
+	<script type="text/javascript" src="js/editarFoto.js"></script>
 
 </body>
 </html>
+
