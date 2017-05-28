44ea1aa036a447b1ef05cbdcb7c2c1e2b00680a4
eliiags <eaguero@ucm.es>
2017-05-29 00:03:30 +0200

    Update edicionFoto.php
    boceto

--- web/edicionFoto.php
+++ web/edicionFoto.php
@@ -8,6 +8,7 @@
 	<link rel="stylesheet" href="css/estilo2.css">
 	<link rel="stylesheet" href="css/estilobusq.css">
 	<link rel="stylesheet" href="css/edicionStyle.css">
+	<link rel="stylesheet" href="css/login.css">
 </head>
 <body>
 	<?php
@@ -16,7 +17,7 @@
 	?>
 
 	<div class="container-fluid text-center">    
-		<form action="php/funciones/editar_foto.php" method="POST" enctype="multipart/form-data">
+		<form id="boceto" action="php/funciones/editar_foto.php" method="POST" enctype="multipart/form-data">
 			<div class="col-sm-10 text-left"> 
 				<div class="panel panel-default margen-top">
 					<div class="panel-heading">
@@ -32,7 +33,7 @@
 								<input type="text" class="form-control" id="titulo" name="titulo" placeholder="Escribe aquí el título"/>
 								<br>
 								<span class="input-group-addon glyphicon glyphicon-text-size">Descripción</span>
-								<input type="text" class="form-control" id="categoria" name="desc" placeholder="Escribe aquí alguna descripción"/>
+								<input type="text" class="form-control" id="desc" name="desc" placeholder="Escribe aquí alguna descripción"/>
 							</div>
 						</div>
 					</div>  
@@ -55,8 +56,10 @@
 	<!--Scripts-->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="js/editarFoto.js"></script>
+	<script type="text/javascript" src="js/edicion_foto.js"></script>
+	<script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>
 
 </body>
 </html>
+
 
