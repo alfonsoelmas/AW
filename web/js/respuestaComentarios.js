20f28ab0c38ebddaf07c160ab5ae833c91edbb0a
eliiags <eaguero@ucm.es>
2017-06-02 12:10:50 +0200

    Add files via upload
    modal

--- web/js/respuestaComentarios.js
+++ web/js/respuestaComentarios.js
@@ -1,7 +1,34 @@
-$(document).ready(function (){
+$(document).ready(function(){
+	
+	$('#form_comment').submit(function(e) { e.preventDefault(); }).validate(
+    { 
+        debug: true,
+        errorClass: "my-error-class",
+        validClass: "my-valid-class",
+        
+        rules: {
+            "cuerpo": {
+                required: true
+            }
+        },
+
+        messages: {
+            "cuerpo": {
+                required: "Comenta esta obra."
+            }
+        },
+
+        submitHandler: function(form){
+            form.submit();
+        }
+    });
+	
 	$('.botones-comentario').click(function(){
 		var id = $(this).attr('data-id');
 		console.log(id);	
 		$('#answerParent').val(id);
 	});
+
+
+
 });
